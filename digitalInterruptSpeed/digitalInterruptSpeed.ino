ddb19d7fde2632c6492142e311a4d4dbfe76ec36
Stick&Wheel <69044919+KshitijKulkarni@users.noreply.github.com>
2025-10-30 22:43:44 +0530

    Initial Commit

---
+++ digitalInterruptSpeed/digitalInterruptSpeed.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(23, INPUT);
+  Serial.begin(9600);
+}
+
+void pulse
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
